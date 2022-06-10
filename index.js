6cf8885058d6f0ac28df151765eefeeeb527725e
mmhuntsberry <mmhuntsberry@gmail.com>
2022-06-09 18:08:38 -0700

    back to where i was

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
-const express = require('express')
+const express = require("express");
 const app = express();
 
-const PORT = 5000;
+const people = require("./data");
 
-app.get('/', (req, res)  => {
-res.send('Hello world')
-})
+const PORT = 8000;
 
-app.listen(PORT, () => console.log(`I am listening on port ${PORT}`))
+app.use(express.json());
+
+app.get("/people", (req, res) => {
+  res.json(people);
+});
+
+app.get("/people/:id", (req, res) => {
+  const id = req.params.id;
+
+  const person = people.find((person) => person.id === Number(id));
+  res.json(person);
+});
+
+app.post("/people", (req, res) => {
+  const person = {
+    id: people.length + 1,
+    ...req.body,
+  };
+
+  console.log(person);
+
+  people.push(person);
+  res.json(person);
+});
+
+app.listen(PORT, () => console.log(`I am listening on port ${PORT}`));
